f667bca962c59a2ba398384ef6cf70b8eee83e70
sssslin <414048771@qq.com>
2020-07-23 15:19:29 +0800

    commit:双重检测锁添加注释

--- src/main/java/ArtConcurrentBook/chapter03/DoubleCheckedLocking.java
+++ src/main/java/ArtConcurrentBook/chapter03/DoubleCheckedLocking.java
@@ -4,10 +4,19 @@ public class DoubleCheckedLocking { //1
     private static Instance instance; //2
 
     public static Instance getInstance() { //3
-        if (instance == null) { //4:��һ�μ��
-            synchronized (DoubleCheckedLocking.class) { //5:����
-                if (instance == null) //6:�ڶ��μ��
-                    instance = new Instance(); //7:����ĸ�Դ��������
+        if (instance == null) { //4:第一次检查
+            synchronized (DoubleCheckedLocking.class) { //5:加锁
+                if (instance == null) //6：第二次检查
+                    instance = new Instance(); //7:问题的根源出在这里
+
+                /**
+                 * 第七步可以分解为以下的伪代码
+                 *（第二步和第三步可能会重排序）
+                 * memory = allocate();// 分配对象的内存空间
+                 * ctorInstance(memory);// 设置instance指向刚分配的内存地址
+                 * 注意，此时对象还没有被初始化
+                 * instance = memory;// 初始化对象
+                 */
             } //8
         } //9
         return instance; //10
