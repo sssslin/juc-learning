8df3aff23173e1f790c7118cd830b8230fb3063a
“sssslin <414048771@qq.com>
2020-07-31 13:00:51 +0800

    commit:countDownLatch注释添加

--- src/main/java/ArtConcurrentBook/chapter04/ConnectionPoolTest.java
+++ src/main/java/ArtConcurrentBook/chapter04/ConnectionPoolTest.java
@@ -8,10 +8,12 @@ import java.util.concurrent.atomic.AtomicInteger;
  * 6-18
  * 线程方法中经常会出现一个表示时间的参数，这样的设计本质上是为了让线程无法获取一些宝贵资源的时候，能够及时返回，而不是一直等待或者阻塞，
  * 是一种自我保护机制
+ *
  */
 public class ConnectionPoolTest {
     static ConnectionPool pool = new ConnectionPool(10);
     // 保证所有ConnectionRunner能够同时开始
+    // 将参数设置为1，countDown后让后续所有线程同时开始，是一种典型的用法
     static CountDownLatch start = new CountDownLatch(1);
     // main线程将会等待所有ConnectionRunner结束后才能执行
     static CountDownLatch end;
@@ -21,16 +23,19 @@ public class ConnectionPoolTest {
         int threadCount = 50;
         // CountDownLatch可以实现类似计数器的功能，只有当count值为0的时候才继续执行
         end = new CountDownLatch(threadCount);
+
         int count = 20;
         AtomicInteger got = new AtomicInteger();
         AtomicInteger notGot = new AtomicInteger();
 
         for (int i = 0; i < threadCount; i++) {
-            Thread thread = new Thread(new ConnetionRunner(count, got, notGot), "ConnectionRunnerThread");
+            Thread thread = new Thread(new ConnectionRunner(count, got, notGot), "ConnectionRunnerThread");
             thread.start();
         }
 
+        // 执行这个方法后，所有的线程开始执行
         start.countDown();
+        // end这个CountDownLatch的作用是，保证所有的线程都执行完，再回到主线程进行统计
         end.await();
 
         System.out.println("total invoke: " + (threadCount * count));
@@ -38,12 +43,12 @@ public class ConnectionPoolTest {
         System.out.println("not got connection " + notGot);
     }
 
-    static class ConnetionRunner implements Runnable {
+    static class ConnectionRunner implements Runnable {
         int count;
         AtomicInteger got;
         AtomicInteger notGot;
 
-        public ConnetionRunner(int count, AtomicInteger got, AtomicInteger notGot) {
+        public ConnectionRunner(int count, AtomicInteger got, AtomicInteger notGot) {
             this.count = count;
             this.got = got;
             this.notGot = notGot;
@@ -51,6 +56,8 @@ public class ConnectionPoolTest {
 
         public void run() {
             try {
+                // 所有线程启动后，都会被start这个CountDownLatch给拦住，无法往下进行，
+                // 只有执行start.countDown()之后，所有的线程都同时继续执行线程
                 start.await();
             } catch (Exception ex) {
 
