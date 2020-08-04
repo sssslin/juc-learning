fd3b57cc14e2932716880335abd91df05b63356d
sssslin <414048771@qq.com>
2020-08-04 15:54:53 +0800

    commit:condition demo提交

---
+++ src/main/java/other/BounderBuffer.java
@@ -0,0 +1,55 @@
+package other;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author junfeng.ling
+ * @date 2020/8/4 9:47
+ * @Description:细节分析在以下这篇博客
+ * https://www.cnblogs.com/hongdada/p/6150699.html
+ */
+public class BounderBuffer {
+
+    final Lock lock = new ReentrantLock();
+
+    // condition依赖于lock产生
+    final Condition notFull = lock.newCondition();
+    final Condition notEmpty = lock.newCondition();
+
+    final Object[] items = new Object[100];
+
+    int putptr, takeptr, count;
+
+    // 生产
+    public void put(Object x) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == items.length)
+                notFull.await();  // 队列已满，等待，直到 not full 才能继续生产
+            items[putptr] = x;
+            if (++putptr == items.length) putptr = 0;
+            ++count;
+            notEmpty.signal(); // 生产成功，队列已经 not empty 了，发个通知出去
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    // 消费
+    public Object take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0)
+                notEmpty.await(); // 队列为空，等待，直到队列 not empty，才能继续消费
+            Object x = items[takeptr];
+            if (++takeptr == items.length) takeptr = 0;
+            --count;
+            notFull.signal(); // 被我消费掉一个，队列 not full 了，发个通知出去
+            return x;
+        } finally {
+            lock.unlock();
+        }
+    }
+}
